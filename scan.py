f0e94de2d474eacbd6bc83dc47a88a55b237aab0
Jiayan Luo <jiayanluo2024@u.northwestern.edu>
2024-03-08 21:33:43 -0600

    basic ipv4 scanner

--- scan.py
+++ scan.py
@@ -11,6 +11,9 @@ Example:
     result = subprocess.check_output(["nslookup", "northwestern.edu", "8.8.8.8"],
                                      timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
 '''
+dns_resolvers = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9",
+                     "64.6.65.6", "91.239.100.100", "185.228.168.168", "77.88.8.7",
+                     "156.154.70.1", "198.101.242.72", "176.103.130.130"]
 
 #
 # Helper Functions
@@ -24,23 +27,25 @@ def read_domains(file):
 #
 def scan_ipv4(domain):
     ipv4_addresses = []
-    dns_resolvers = ["208.67.222.222", "1.1.1.1", "8.8.8.8", "8.26.56.26", "9.9.9.9",
-                     "64.6.65.6", "91.239.100.100", "185.228.168.168", "77.88.8.7",
-                     "156.154.70.1", "198.101.242.72", "176.103.130.130"]
     ip_pattern = r"Address:\s*(\d+\.\d+\.\d+\.\d+)"
 
     for resolver in dns_resolvers:
         try:
-            nslookup = subprocess.check_output(["nslookup", "northwestern.edu", resolver],
+            nslookup = subprocess.check_output(["nslookup", domain, resolver],
                                             timeout=2, stderr=subprocess.STDOUT).decode("utf-8")
             addresses = re.findall(ip_pattern, nslookup)
-            ipv4_addresses = ipv4_addresses + addresses
-            break
+            ipv4_addresses.extend(addresses)
         except subprocess.TimeoutExpired:
+            print(f"Timeout expired for resolver {resolver}")
             continue
-
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            continue
+            
     return list(set(ipv4_addresses))
 
+def scan_ipv6(domain):
+    pass
 
 def scan_domains(domains):
     result = {}
